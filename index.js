dd22c4376c51fd31c3040c2af21cc090182228a1
okku0916 <naoki.okuda.7m@stu.hosei.ac.jp>
2026-02-26 15:58:29 +0900

    add main

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const { MongoClient } = require('mongodb' );
+const client = new
+	MongoClient ('mongodb://localhost:27017' );
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// publicディレクトリ以下のファイルを静的ファイルとして配信
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+async function main() {
+	// サーバーのlisten前に接続する
+	await client.connect();
+	const db = client.db('my-app' );
+
+	app.get('/', async (req, res) => {
+		// dbから取ってくるとオブジェクトで返ってくるからnamesという配列にする
+		const users = await db.collection('user').find().toArray();
+		const names = users.map((user) => { return user.name });
+		res.render(path.join(__dirname, 'views','index.ejs'), { users: names });
+	});
+
+	app.listen(3000, () => {
+		console.log('start listening' );
+	});
+}
+main();
